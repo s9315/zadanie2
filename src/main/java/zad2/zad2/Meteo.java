bf48cb58ced0c4e69a99431bf3f1ffaa9896a033
student <student@student-VirtualBox>
2012-11-28 11:41:52 +0100

    a

--- src/main/java/zad2/zad2/Meteo.java
+++ src/main/java/zad2/zad2/Meteo.java
@@ -1,13 +1,20 @@
 package zad2.zad2;
 
+import java.io.IOException;
+
 public class Meteo {
 
 
-	public static void main(String[] args){
-		DanePogodowe danePogodowe = new DanePogodowe;
-		Wyswietlanie prognozaWyswietl = new Wyswietlanie(); 
+
+	private static final DanePogodowe  = null;
+
+	public static void main(String[] args)throws IOException{
+		DanePogodowe x = new DanePogodowe();
+		Wyswietlanie y = new Wyswietlanie(x); 
 		
-		
+		x.Odczyty(26, 58, 1060);
+		x.Odczyty(25.5, 44, 966);
+		x.Odczyty(23.4, 88, 1210);
 	}
 
 }
